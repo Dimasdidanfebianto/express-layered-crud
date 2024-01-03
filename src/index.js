0486eba7aec7e198cca602df3338fc4f643f894b
Dimasdidanfebianto <dimaskanjeng113@gmail.com>
2024-01-03 22:39:53 +0700

    update: clean code

--- src/index.js
+++ src/index.js
@@ -7,102 +7,11 @@ const prisma = new PrismaClient()
 const app = express()
 dotenv.config();
 
+const productController = require('./product/product.controller')
+
 const PORT = process.env.PORT;
 app.use(express.json())
 
-
-app.get("/", (req, res) => {
-    res.send("Hello World");
-});
-
-app.get("/products", async (req, res) => {
-    const products = await prisma.product.findMany();
-
-    res.send(products);
-});
-
-app.get("/products/:id", as)
-
-app.post("/products", async (req, res) => {
-    const newProductData = req.body;
-
-    const product = await prisma.product.create({
-        data: {
-            name: newProductData.name,
-            description: newProductData.description,
-            price: newProductData.price,
-            image: newProductData.image,
-        }
-    });
-    res.status(200).send({
-        data: product,
-        message: "Product created successfully",
-        
-    })
-});
-
-app.put("/products/:id", async (req, res) => {
-    const { id } = req.params;
-    const { name, description, price, image } = req.body;
-
-    if (productData.image && productData.description && productData.price && productData.name) {
-        return res.status(400).send({
-            message: "All fields are required",
-        });
-    }
-
-    const product = await prisma.product.update({
-        where: {
-            id: Number(id),
-        },
-        data: {
-            name,
-            description,
-            price,
-            image,
-        },
-    });
-    res.status(200).send({
-        data: product,
-        message: "Product updated successfully",
-    })
-})
-
-app.delete("/products/:id", async (req, res) =>{
-    const { id } = req.params;
-    const product = await prisma.product.delete({
-        where: {
-            id: Number(id),
-        },
-    });
-    res.status(200).send({
-        data: product,
-        message: "Product deleted successfully",
-
-    })
-})
-
-app.patch("/products/:id", async (req, res) =>{
-    const { id } = req.params;
-    const { name, description, price, image } = req.body;
-
-    const product = await prisma.product.update({
-        where: {
-            id: Number(id),
-        },
-        data: {
-            name,
-            description,
-            price,
-            image,
-        },
-    })
-    res.status(200).send({
-        data: product,
-        message: "Product updated successfully",
-    })
-})
-
-
+app.use('/products', productController)
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
